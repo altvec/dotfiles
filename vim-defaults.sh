959498a7ace5da6076711969eea3066fda9f9193
altvec <sergey@bluehat.me>
2014-01-23 01:33:15 +0600

    Cleared some options, added descriptions

--- vim-defaults.sh
+++ vim-defaults.sh
@@ -14,46 +14,48 @@
 #
 # And get same sane default for vim.
 
-VIMRC="set nocompatible
-set hidden
-set nowrap
-set tabstop=2
-set backspace=indent,eol,start
+VIMRC='
+set t_Co=256                                " use 256 colors by default
+set background=dark                         " set dark background
+set tabstop=4                               " number of visual spaces per TAB
+set softtabstop=4                           " number of spaces in tab when editing
+set expandtab                               " tabs are spaces
+
+set number                                  " show line numbers
+set showcmd                                 " show command in bottom bar
+set cursorline                              " highlight current line
+
+filetype indent on                          " load filetype-specific indent file
+
+set wildmenu                                " visual autocomplete for command menu
+set lazyredraw                              " redraw only when we need to
+set showmatch                               " highlight matching [{()}]
+
+set incsearch                               " search as characters are entered
+set hlsearch                                " highlight matches
+
+nnoremap <leader><space> :nohlsearch<CR>    " turn off search hightlight: press ,<space>
+nnoremap <space> za                         " space open/closes folds
+
+set foldenable                              " enable folding
+set foldlevelstart=10                       " open most folds by default
+set foldnestmax=10                          " 10 nested folds max
+set foldmethod=indent                       " fold based on indent level
+
+let mapleader=","                           " leader is comma
+
+set nocompatible                            " default to vim
+set nowrap                                  " disable line wrap
 set autoindent
 set copyindent
-set shiftwidth=2
-set shiftround
-set showmatch
-set ignorecase
-set smartcase
 
-set smarttab
-
-set hlsearch
-set incsearch
-set textwidth=79
-set nocursorline
-set nojoinspaces
-set cpoptions+=J
-set noswapfile
+set textwidth=79                            " line width set to 79 symbols
+set noswapfile                              " its 2014, disable swap file
 
 filetype on
 filetype plugin on
-filetype indent on
 filetype plugin indent on
-set expandtab
 
-nnoremap ; :
-map <C-h> <C-w>h
-map <C-j> <C-w>j
-map <C-k> <C-w>k
-map <C-l> <C-w>l
-
-set ruler
-nnoremap K <nop>
-
-nnoremap <Space> za
-vnoremap <Space> za
-syntax enable"
-
+syntax enable                               " enable syntax processing
+'
 echo "$VIMRC" > $HOME/.vimrc
