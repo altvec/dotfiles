fbd884b61f30c465e34ddb2ec6e611a40d636095
altvec <sergey@bluehat.me>
2015-01-01 17:40:25 +0500

    Better OS X settings -- initial

---
+++ better-osx.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# A lot of these things have been taken from the variuos places on the web, most
+# from here: https://github.com/mathiasbynens/dotfiles/blob/master/.osx and
+# https://bitbucket.org/sjl/dotfiles/raw/603bb1ae9da27c6e08ab115df1cb5d8f6a1442c3/osx.sh
+
+
