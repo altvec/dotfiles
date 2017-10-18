1d7a9aa1802abccc147ae5ebdba1c9917ef0b4d6
_srg <me@sergeykalistratov.com>
2017-10-18 23:40:19 +0500

    Emacs tweaks

---
+++ emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (jedi ob-python org-babel-eval-in-repl python-mode solaire-mode twilight-bright-theme neotree exec-path-from-shell evil elpy doom-themes better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -33,10 +33,16 @@
     exec-path-from-shell
     evil
     elpy
+    jedi
     twilight-bright-theme
     doom-themes
+    solaire-mode
     all-the-icons
-    neotree))
+    neotree
+    slime
+    org-bullets
+    ob-ipython
+    python-mode))
 
 (mapc #'(lambda (package)
 	  (unless (package-installed-p package)
@@ -61,41 +67,40 @@
       '((width . 92)
 	(height . 54)))
 
+
 ;; --- Theme and Appearance {{
-
-;; For somethin' more lightweight
-(require 'twilight-bright-theme)
-(load-theme 'twilight-bright t)
-
 (set-face-attribute 'default nil :family "Operator Mono Light")
 (setq-default line-spacing 3)
 (set-face-attribute 'default nil :height 120)
 (setq-default cursor-type 'bar)
 
+(require 'all-the-icons)
+(require 'doom-themes)
+(setq doom-themes-enable-bold t
+      doom-themes-enable-italic t)
 
-;; Doom theme
-;(require 'doom-themes)
-;(setq doom-themes-enable-bold t
-;      doom-themes-enable-italic t)
+(load-theme 'doom-molokai t)
+(add-hook 'find-file-hook 'solaire-mode)  ; set doom-buffers to be 'brighter'
+(defvar doom-ui-theme 'doom-molokai)
 
-;; load theme
-;(load-theme 'doom-one t)
-;(add-hook 'find-file-hook 'doom-buffer-mode)  ; set doom-buffers to be 'brighter'
-;(defvar doom-ui-theme 'doom-one)
-
-;; enable flashig mode-line on errors
-;(doom-themes-visual-bell-config)
-
-;; enable custom neotree theme
-;;(require 'doom-neotree)
-;;(setq doom-neotree-enable-file-icons 'simple)
+(doom-themes-visual-bell-config)
 
 ;; enable line numbers and highlight the current line
 (global-linum-mode t)
 (global-hl-line-mode 1)
-
-(require 'all-the-icons)
-
+(unless window-system
+  (add-hook 'linum-before-numbering-hook
+	    (lambda ()
+	      (setq-local linum-format-ftm
+			  (let ((w (length (number-to-string
+					    (count-lines (point-min) (point-max))))))
+			    (concat "%" (number-to-string w) "d"))))))
+(defun linum-format-func (line)
+  (concat
+   (propertize (format linum-format-ftm line) 'face 'linum)
+   (propertize " " 'face 'mode-line)))
+(unless window-system
+  (setq linum-format 'linum-format-func))
 ;; --- }}
 
 ;; Turn off mouse interface early in startup to avoid momentary display
@@ -134,12 +139,27 @@
 		  (join-line -1)))
 
 ;; --- Python configuration {{{
+(require 'jedi)
+(add-to-list 'ac-sources 'ac-source-jedi-direct)
+(add-hook 'python-mode-hook 'jedi:setup)
+
 (elpy-enable)
 (elpy-use-ipython)
 ;; --- }}}
 
+;; --- LISP {{{
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-contrib '(slime-fancy))
+;; --- }}}
 
-;; =========================================
-;; Various stuff atomatically added by Emacs
-;; =========================================
+;; --- Org mode {{{
+(require 'org-bullets)
+(require 'ob-ipython)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
+(org-babel-do-load-languages
+ 'org-label-load-languages
+ '((ipython . t)
+   (lisp . t)
+   ))
+;; --- }}}
