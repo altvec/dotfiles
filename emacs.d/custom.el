0f252fb1dc97137fb5c58bc8994308c1e21257f1
_srg <me@sergeykalistratov.com>
2017-10-21 02:49:56 +0500

    emacs stuff

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (jedi ob-python org-babel-eval-in-repl python-mode solaire-mode twilight-bright-theme neotree exec-path-from-shell evil elpy doom-themes better-defaults))))
+    (all-the-icons-ivy evil-search evil-search-highlight-persist jedi ob-python org-babel-eval-in-repl python-mode solaire-mode twilight-bright-theme neotree exec-path-from-shell evil elpy doom-themes better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
