a4317ffece6f801f65511e088217986c5f662bb9
_srg <me@sergeykalistratov.com>
2018-08-16 22:24:56 +0500

    More stuff

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -236,8 +236,16 @@
 
   (setq org-src-fontify-natively t)
 
-  (custom-set-variables
-    '(org-export-backends (quote (ascii html icalendar latex md odt))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-export-backends (quote (ascii html icalendar latex md odt)))
+ '(package-selected-packages
+   (quote
+    (yaml-mode magit simpleclip helm-ag which-key use-package try org-bullets nyan-mode helm-projectile expand-region exec-path-from-shell beacon))))
 
   (find-file "~/org/main.org")
 
@@ -254,3 +262,9 @@
     :ensure t
     :config
     (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
